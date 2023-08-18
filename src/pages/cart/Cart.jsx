bec06ee3f90432b942a3e95262108fa58ed9711f
TheSh4h <hh.haseebshah@gmail.com>
2023-08-18 11:30:53 -0700

    cart setup

--- src/pages/cart/Cart.jsx
+++ src/pages/cart/Cart.jsx
@@ -1,7 +1,24 @@
+import { useContext } from "react";
+import { PRODUCTS } from "../../products";
+import { ShopContext } from "../../context/shop-context";
+import CartItem from "./Cart-Item";
+import "./cart.css";
+
 const Cart = () => {
+    const { cartItems } = useContext(ShopContext);
     return ( 
-        <>
-        </>
+        <div className="cart">
+            <div>
+                <h1> Your Cart Items </h1>
+            </div>
+            <div className="cartItems">
+                {PRODUCTS.map((product) => {
+                    if (cartItems[product.id] !== 0) {
+                        return <CartItem data ={product} />
+                    }
+                })}
+            </div>
+        </div>
      );
 }
  
